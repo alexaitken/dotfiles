9a76b40c44b187be6d218fff98ec602014b184c3
Alex Aitken <alex@alexaitken.com>
2014-05-31 19:39:25 +0000

    Fix up vagrant install script.

--- extras/shopify-vm.sh
+++ extras/shopify-vm.sh
@@ -1,20 +1,28 @@
 if ! vim --version | grep -q "+ruby"; then
+  VIM_VERSION='7.4'
   echo "Installing Vim 7.4 with Ruby support..."
   (
-  cd /tmp
-  source /usr/local/share/chruby/chruby.sh
-  ruby --version
+    cd /tmp
+    source /usr/local/share/chruby/chruby.sh
+    ruby --version
 
-  wget "ftp://ftp.vim.org/pub/vim/unix/vim-$VIM_VERSION.tar.bz2"
-  tar xjf "vim-$VIM_VERSION.tar.bz2"
-  cd vim74
-  ./configure --enable-rubyinterp
-  make --jobs "$(cores)"
-  sudo make install
+    wget "ftp://ftp.vim.org/pub/vim/unix/vim-$VIM_VERSION.tar.bz2"
+    tar xjf "vim-$VIM_VERSION.tar.bz2"
+    cd vim74
+    ./configure --enable-rubyinterp
+    make --jobs "$(cores)"
+    sudo make install
   )
 
   # Refresh links
   hash -r
+fi
+# Clone vundle into the directory if it's not already
+if [[ ! -e ~/.vim/bundle/vundle/.git ]]
+then
+  mkdir -p ~/.vim/bundle
+  rm -rf ~/.vim/bundle/vundle
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
 vim +BundleInstall +qall
@@ -28,7 +36,7 @@ echo "Compiling commandt..."
   make
 )
 
-if ! tmux --version | grep -q "1.9a"; then
+if ! tmux -V | grep -q "1.9a"; then
   cd /tmp
   wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz
 
